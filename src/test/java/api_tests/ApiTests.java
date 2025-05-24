655573d82041a318bbe6d85965efe3a6eba297d6
davgal <davvv.galstyan@gmail.com>
2025-05-24 14:32:37 +0400

    v2

--- src/test/java/api_tests/ApiTests.java
+++ src/test/java/api_tests/ApiTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-@Tag("api")
+@Tag("API")
 public class ApiTests {
 
     static {
